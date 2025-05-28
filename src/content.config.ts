7b638cfdd51bd519355fc411a3379d676c5c42bc
Jeroen Beekmans <jeroen@popupcity.net>
2025-05-28 15:34:46 +0200

    Update Astro, create content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,18 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
 
-// 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
-
-// 3. Define your collection(s)
-const verhalen = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/verhalen' }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    location: z.object({
-      lat: z.number(),
-      lng: z.number(),
-    }),
-    audio: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
+const verhalenSchema = z.object({
+  title: z.string(),
+  location: z.object({
+    lat: z.number(),
+    lng: z.number(),
   }),
+  audio: z.string(),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
-export const collections = { verhalen };
+const verhalen = defineCollection({
+  schema: verhalenSchema,
+});
+
+export const collections = {
+  verhalen,
+};
